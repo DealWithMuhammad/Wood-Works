4966b54e8a0e0023c9d9a2a60be8d3e42f25bc04
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2023-12-13 20:20:00 +0800

    ApiTesting

--- utils/sanity.client.ts
+++ utils/sanity.client.ts
@@ -1,13 +1,11 @@
 import { ClientConfig, createClient } from 'next-sanity';
 
-
 export const clientConfig: ClientConfig = {
   projectId: '1nffi0j5',
   dataset: 'production',
   useCdn: false,
   apiVersion: '2023-03-23',
-  token:
-    "skfDOXGryrIeLRG8696qjP64IgOYK2FRY5JpdJwUYDhshgrfaV3YzMWPeVatS2fvnJTr5lpsEoBoXUhfRSmIjTloK6jXTyfhqoiaTfUgrKoOwDAyXanvW0cUIfttuWtvsvw9BEjwOyv3QyprfS8v8MjQOuSU7we5p3ooGBfqSMlu33RBUaVD"
+  token:"skfDOXGryrIeLRG8696qjP64IgOYK2FRY5JpdJwUYDhshgrfaV3YzMWPeVatS2fvnJTr5lpsEoBoXUhfRSmIjTloK6jXTyfhqoiaTfUgrKoOwDAyXanvW0cUIfttuWtvsvw9BEjwOyv3QyprfS8v8MjQOuSU7we5p3ooGBfqSMlu33RBUaVD"
 };
 
 export const client = createClient(clientConfig);
