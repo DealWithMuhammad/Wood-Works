3be8ee45aa6ea4e073ef39b14c342cdb3a2f3c85
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2023-12-13 20:40:32 +0800

    testing

--- utils/sanity.client.ts
+++ utils/sanity.client.ts
@@ -5,6 +5,7 @@ export const clientConfig: ClientConfig = {
   dataset: 'production',
   useCdn: false,
   apiVersion: '2023-03-23',
-  token:process.env.SANITY_API_TOKEN };
+  // token:process.env.SANITY_API_TOKEN || "skfDOXGryrIeLRG8696qjP64IgOYK2FRY5JpdJwUYDhshgrfaV3YzMWPeVatS2fvnJTr5lpsEoBoXUhfRSmIjTloK6jXTyfhqoiaTfUgrKoOwDAyXanvW0cUIfttuWtvsvw9BEjwOyv3QyprfS8v8MjQOuSU7we5p3ooGBfqSMlu33RBUaVD"
+};
 
 export const client = createClient(clientConfig);
