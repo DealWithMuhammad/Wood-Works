d71dfcdcb2232f2feed110626b47d9ffeb76468d
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2023-12-15 01:46:32 +0800

    removed grid and added flex in categories

--- src/features/categories/index.tsx
+++ src/features/categories/index.tsx
@@ -1,28 +1,28 @@
-'use client';
-import { Card, CardBody, Grid, Heading } from '@chakra-ui/react';
-import { ICategory } from '@src/model';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import { Card, CardBody, Flex, Grid, Heading } from "@chakra-ui/react";
+import { ICategory } from "@src/model";
+import Image from "next/image";
+import Link from "next/link";
 
 interface AllCategoriesProps {
   categories: ICategory[];
 }
 export const AllCategories = ({ categories }: AllCategoriesProps) => {
   return (
-    <Grid
+    <Flex
       py="2rem"
-      w={{ base: '100%', lg: '90%' }}
-      templateColumns={{
-        base: 'repeat(1, 1fr)',
-        lg: 'repeat(2, 1fr)',
-      }}
-      gap="20px"
+      w={{ base: "100%", lg: "90%" }}
+      direction={{ base: "column", lg: "row" }}
+      justify="space-around"
+      align="center"
+      gap={10}
       mx="auto"
+      flexWrap="wrap" // Allow flex items to wrap to the next line
     >
       {categories.map((category) => (
         <CategoryCard key={category.id} category={category} />
       ))}
-    </Grid>
+    </Flex>
   );
 };
 
@@ -37,20 +37,18 @@ const CategoryCard = ({ category }: CategoryCardProps) => (
       align="center"
       overflow="hidden"
       variant="outline"
-      w="100%"
       p="10px"
-      h="100%"
-      _hover={{ cursor: 'pointer', bgColor: 'gray.100' }}
+      _hover={{ cursor: "pointer", bgColor: "gray.100" }}
     >
       <Image
         src={category.image}
         alt={category.name}
-        height={200}
-        width={200}
+        width={450}
+        height={100}
       />
 
       <CardBody>
-        <Heading size={{ base: 'sm', lg: 'md' }}>{category.name}</Heading>
+        <Heading size={{ base: "sm", lg: "md" }}>{category.name}</Heading>
       </CardBody>
     </Card>
   </Link>
