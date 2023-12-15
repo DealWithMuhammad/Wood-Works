eda86b6da0894e6dc55f32ace775ed3aa656bcd8
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2023-12-16 00:46:57 +0800

    background color of body changed

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,31 +1,31 @@
-'use client'
-import 'swiper/css';
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '../../theme'
-import { Navbar } from '@src/components/Navbar/NavBar'
-import { Footer } from '@src/components/Footer'
+"use client";
+import "swiper/css";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../../theme";
+import { Navbar } from "@src/components/Navbar/NavBar";
+import { Footer } from "@src/components/Footer";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
         <title>SW Work</title>
         <meta title="description" content="Buy any furniture" />
       </head>
-    <body>
-     <CacheProvider>
+      <body style={{ backgroundColor: "#f4f3ef" }}>
+        <CacheProvider>
           <ChakraProvider theme={theme}>
             <Navbar />
             {children}
-              <Footer />
-        </ChakraProvider>
-       </CacheProvider>
-    </body>
-  </html>
-  )
+            <Footer />
+          </ChakraProvider>
+        </CacheProvider>
+      </body>
+    </html>
+  );
 }
